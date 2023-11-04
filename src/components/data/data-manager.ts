d1c50e2cbda28e5686850cebd784f0b545cfe774
Christopher Clark <clark-chris@comcast.net>
2023-11-04 16:23:36 -0400

    Added a debug menu that logs all the workout data.

--- src/components/data/data-manager.ts
+++ src/components/data/data-manager.ts
@@ -1,7 +1,8 @@
 import { Routine, RoutineSerial, RoutineSerializer } from './routine';
 import { LocalStorage } from 'quasar';
+import { Workout } from './workout';
 
-interface DataSerial {
+export interface DataSerial {
   routines: RoutineSerial[];
 }
 
@@ -26,10 +27,64 @@ export class AppData {
   }
 
   public save() {
-    const serialData: DataSerial = {
+    const serialData: DataSerial = this.serialize();
+    LocalStorage.set(AppData.workoutsKey, serialData);
+  }
+  public serialize(): DataSerial {
+    return {
       routines: this.routines.map((r) => this.routineSerializer.serialize(r)),
     };
-    LocalStorage.set(AppData.workoutsKey, serialData);
+  }
+
+  public mergeWorkouts(existingWorkouts: Workout, incomingWorkout: Workout) {
+    for (const incomingSet of incomingWorkout.sets) {
+      const existingSet = existingWorkouts.sets.find(
+        (s) => s.uuid === incomingSet.uuid
+      );
+
+      if (!existingSet) {
+        existingWorkouts.sets.push(incomingSet);
+      } else {
+        existingSet.repCount = incomingSet.repCount;
+        existingSet.liftWeight = incomingSet.liftWeight;
+        existingSet.weightUnits = incomingSet.weightUnits;
+        existingSet.recordTimestamp = incomingSet.recordTimestamp;
+      }
+    }
+  }
+
+  public mergeRoutines(existingRoutine: Routine, incomingRoutine: Routine) {
+    for (const incomingWorkout of incomingRoutine.workouts) {
+      const existingWorkout = existingRoutine.workouts.find(
+        (w) => w.uuid === incomingWorkout.uuid
+      );
+
+      if (!existingWorkout) {
+        existingRoutine.workouts.push(incomingWorkout);
+      } else {
+        existingWorkout.weightUnits = incomingWorkout.weightUnits;
+        this.mergeWorkouts(existingWorkout, incomingWorkout);
+      }
+    }
+  }
+
+  public mergeData(routines: Routine[]) {
+    for (const incomingRoutine of routines) {
+      const existingRoutine = this.routines.find(
+        (r) => r.uuid === incomingRoutine.uuid
+      );
+      const existingRoutineName = this.routines.find(
+        (r) => r.name === incomingRoutine.name
+      );
+
+      if (!existingRoutine) {
+        this.routines.push(incomingRoutine);
+      } else {
+        existingRoutine.configuration = incomingRoutine.configuration;
+        existingRoutine.name = incomingRoutine.name;
+        this.mergeRoutines(existingRoutine, incomingRoutine);
+      }
+    }
   }
 
   private synchTimer(): void {
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -27,14 +27,23 @@
       >
         <q-tab key="data" label="Data" name="data" />
         <q-tab key="graphs" label="Graphs" name="graphs" />
+        <q-tab key="debug" label="Debug" name="debug"></q-tab>
       </q-tabs>
 
-      <q-btn
-        flat
-        icon="upload_file"
-        aria-label="Upload File"
-        @click="openFileDialog"
-      />
+      <q-btn-dropdown flat label="File">
+        <q-list>
+          <q-item>
+            <q-item-section>
+              <q-item-label @click="openFileDialog">Load File</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section>
+              <q-item-label @click="saveFile">Save File</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
 
       <input
         ref="fileInput"
@@ -43,6 +52,14 @@
         accept=".json"
         @change="loadFile"
       />
+      <!-- <input
+        type="file"
+        id="ctrl"
+        webkitdirectory
+        directory
+        multiple
+        text="Abc"
+      /> -->
     </div>
     <div v-if="tabSelected === 'data'" class="main-body">
       <workouts-table
@@ -50,7 +67,10 @@
         :key="RoutineNavigator.selectedRoutine?.uuid"
       ></workouts-table>
     </div>
-    <main-graphs v-else-if="(tabSelected = 'graphs')"></main-graphs>
+    <main-graphs v-else-if="tabSelected === 'graphs'"></main-graphs>
+    <div v-else-if="tabSelected === 'debug'">
+      <textarea v-text="debugMsg" style="width: 100%; height: 100vh"></textarea>
+    </div>
   </div>
 </template>
 
@@ -65,6 +85,15 @@ import { AppData } from 'src/components/data/data-manager';
 import DivTable from 'components/DivTable.vue';
 import MainGraphs from 'components/MainGraphs.vue';
 import { RoutineSerializer } from 'components/data/routine';
+import { DataSerial } from 'src/components/data/data-manager';
+import { DateTime } from 'luxon';
+import { exportFile } from 'quasar';
+import { resolveLocalFileSystemURI } from 'app/src-cordova/platforms/android/platform_www/plugins/cordova-plugin-file/www/resolveLocalFileSystemURI';
+import { json } from 'stream/consumers';
+
+// let i = 0;
+// crypto.randomUUID = () => `${i++}-${i++}-${i++}-${i++}-${i++}`;
+// declare const LocalFileSystem: any;
 
 export default defineComponent({
   name: 'IndexPage',
@@ -86,6 +115,8 @@ export default defineComponent({
         },
       }),
       tabSelected: 'data' as 'data' | 'graphs',
+      downloadAllowed: undefined as any,
+      debugMsg: '',
     };
   },
   methods: {
@@ -93,13 +124,115 @@ export default defineComponent({
       this.leftDrawerOpen = !this.leftDrawerOpen;
     },
     async loadFile(input: any) {
-      const routines: Routine[] = (
-        JSON.parse(await input.target.files[0].text()) as RoutineSerial[]
-      ).map((r) => new RoutineSerializer().deserialize(r));
-      AppData.singleton.routines.push(...routines);
+      const serializedData: DataSerial = JSON.parse(
+        await input.target.files[0].text()
+      );
+      const routines: Routine[] = serializedData.routines.map((r) =>
+        new RoutineSerializer().deserialize(r)
+      );
+      AppData.singleton.mergeData(routines);
+      AppData.singleton.save();
     },
     openFileDialog() {
       (this.$refs.fileInput as any).click();
+    },
+    async saveFile() {
+      AppData.singleton.save();
+
+      // new FileReader().readAsTex;
+      // const directory =
+      //   ((cordova as unknown as { file: Record<string, string> }).file
+      //     .externalRootDirectory ??
+      //     (cordova as unknown as { file: Record<string, string> }).file
+      //       .dataDirectory) + 'Download/test';
+
+      console.log = (...msgs: any[]) => {
+        this.debugMsg += `\n>${msgs.join(' ')}`;
+      };
+      console.log(JSON.stringify(AppData.singleton.serialize()));
+
+      // console.log(
+      //   'File manager result: ',
+      //   await (window as any).FileManager.createFile(
+      //     'filename.txt',
+      //     'file contents'
+      //   )
+      // );
+      return;
+
+      // console.log(
+      //   'file locations: ',
+      //   JSON.stringify(
+      //     (cordova as unknown as { file: Record<string, string> }).file,
+      //     undefined,
+      //     ' '
+      //   )
+      // );
+      // console.log('directory: ', directory);
+
+      // window.resolveLocalFileSystemURL(
+      //   directory,
+      //   function (this: any, f: Entry) {
+      //     console.log('file success: ', f.fullPath);
+      //     console.log('file success: ', f);
+      //     console.log('file success: ', JSON.stringify(f));
+      //   }.bind(this),
+      //   (f) => {
+      //     console.log('file error', f);
+      //   }
+      // );
+
+      // const fileSystem = window.requestFileSystem(
+      //   1,
+      //   1024,
+      //   function (this: any, fs: FileSystem) {
+      //     console.log('fs:', fs);
+      //     fs.root.getDirectory(
+      //       directory,
+      //       undefined,
+      //       (d) => {
+      //         console.log('directory success:', d);
+      //       },
+      //       (e) => {
+      //         console.log('directory fail:', JSON.stringify(e as DOMException));
+      //       }
+      //     );
+
+      //     // this.cordova = fileSystem;
+      //   }.bind(this)
+      // );
+
+      // return;
+
+      // var textToWrite = JSON.stringify(AppData.singleton.serialize());
+
+      // var textFileAsBlob = new Blob([textToWrite], { type: 'text/json' });
+      // var fileNameToSaveAs = `workout-data-${DateTime.now().toFormat(
+      //   'yyyyMMdd_hhmmss'
+      // )}.json`; //filename.extension
+
+      // const allowed = exportFile(fileNameToSaveAs, textToWrite, {
+      //   mimeType: 'text/json',
+      // });
+      // this.downloadAllowed = allowed;
+      // console.log('exportFile: ', allowed);
+
+      // // var downloadLink = document.createElement('a');
+      // // downloadLink.download = fileNameToSaveAs;
+      // // downloadLink.innerHTML = 'Download File';
+      // // if (window.webkitURL != null) {
+      // //   // Chrome allows the link to be clicked without actually adding it to the DOM.
+      // //   downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+      // // } else {
+      // //   // Firefox requires the link to be added to the DOM before it can be clicked.
+      // //   downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+      // //   // downloadLink.onclick = (event) =>
+      // //   //   event.target ? document.body.removeChild(event.target) : undefined;
+      // //   downloadLink.style.display = 'none';
+      // //   document.body.appendChild(downloadLink);
+      // // }
+
+      // // downloadLink.click();
     },
   },
 
@@ -109,7 +242,10 @@ export default defineComponent({
       this.$forceUpdate();
     });
     const routineTableRef = this.$refs['routine-table'] as any;
-    routineTableRef?.selectRow?.(RoutineNavigator.getSelectedRoutine());
+    const routineToSelect = RoutineNavigator.getSelectedRoutine();
+    if (routineToSelect) {
+      routineTableRef?.selectRow?.(routineToSelect);
+    }
   },
 
   setup() {
